c360576353030365337b5e8572a225fa6a6f9249
Kevin Ketterer <ketterer.kevin@gmail.com>
2015-09-29 19:04:04 -0500

    Initial Commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using AccountOperations.Models;
+using System.Data.SqlClient;
+
+namespace AccountOperations.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+        public ActionResult MasterDataMaint()
+        {
+            ViewBag.Message = "Master Data Maintenance.";
+            
+            IList<Projects> ProjectsList = GetProjects().ToList();
+            return View(ProjectsList);
+        }
+
+        public List<Projects> GetProjects()
+        {
+            string connectionString = System.Configuration.ConfigurationManager.ConnectionStrings["accountOpsConn"].ConnectionString;
+            List<Projects> ProjectsList = new List<Projects>();
+
+            using (var conn = new SqlConnection(connectionString))
+            using (var cmd = conn.CreateCommand())
+            {
+                conn.Open();
+                cmd.CommandText = DBQueries.GetProjects;
+                using (var reader = cmd.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        Projects temp = new Projects();
+                        temp.projectId = reader.GetInt32(reader.GetOrdinal("PROJ_ID"));
+                        temp.projectName = reader.GetString(reader.GetOrdinal("PROJ_NM"));
+                        temp.projectDesc = reader.GetString(reader.GetOrdinal("PROJ_DESC"));
+                        temp.projectBeginDate = reader.GetDateTime(reader.GetOrdinal("PROJ_BEG_DT"));
+                        temp.projectEndDate = reader.GetDateTime(reader.GetOrdinal("PROJ_END_DT"));
+                        temp.lastUpdatedBy = reader.GetInt32(reader.GetOrdinal("LAST_UPD_BY"));
+                        temp.lastUpdatedTime = reader.GetDateTime(reader.GetOrdinal("LAST_UPD_TS"));
+
+                        ProjectsList.Add(temp);
+
+                    };
+                                    }
+            }
+            return ProjectsList;
+        }
+    }
+}
---
+++ Models/Projects.cs
@@ -0,0 +1,22 @@
+﻿using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Web;
+//using System.Text;
+//using System.IO;
+//using System.Data.SqlClient;
+//using System.Data;
+
+namespace AccountOperations.Models
+{
+    public class Projects
+    {
+        public int projectId { get; set; }
+        public String projectName { get; set; }
+        public String projectDesc { get; set; }
+        public DateTime projectBeginDate { get; set; }
+        public DateTime projectEndDate { get; set; }
+        public int lastUpdatedBy { get; set; }
+        public DateTime lastUpdatedTime { get; set; }
+    }
+}
